add83d300446d9bf5bf7355443df9765dd9ac56b
Kishore <kishore07kzr@gmail.com>
2024-12-24 14:19:55 +0530

    feat: add data creation

--- src/app.js
+++ src/app.js
@@ -6,10 +6,20 @@ async function main() {
     try {
         await connectMongoose();
         const app = express();
-        app.post("/user", (req, res) => {
-            const user = req.body;
+        app.post("/user", async(req, res) => {
+            try {
+                console.log("got request");
+                const user = req.body;
+                const userModel = new User(user);
+                await userModel.save();
+                res.status(200).send("User created");
+            } catch (error) {
+                console.log(error);
+                res.status(500).send("Unable to create user");
+            }
+
         })
-        app.listen("3000", () => {
+        app.listen("3100", () => {
             console.log("Server up and runnin")
         });
     } catch (error) {
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const { stringify } = require('querystring');
 
 const userData = {
     name: String,
@@ -8,4 +7,4 @@ const userData = {
 }
 const userSchema = new mongoose.Schema(userData)
 const User = new mongoose.model("User", userSchema);
-module.exports(User);
+module.exports = User;
