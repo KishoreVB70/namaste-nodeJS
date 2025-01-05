e2e44dad2deaa72e34805b9f05a8726c135f7d43
Kishore <kishore07kzr@gmail.com>
2025-01-05 09:19:46 +0530

    feat: add user validation

---
+++ src/utils/validation.js
@@ -0,0 +1,24 @@
+function userValidation(user) {
+    if (!user.name || !user.password || !user.image || !user.email) {
+        return false;
+    }
+
+    if (!usernameValidation) return false;
+    if (!passwordValidation) return false;
+    if (!emailValidation) return false;
+    return true;
+}
+
+function usernameValidation(name) {
+    if (name.length < 3 || name.length > 14) return false;
+}
+
+function passwordValidation() {
+    return true;
+}
+
+function emailValidation() {
+    return true;
+}
+
+module.exports = {userValidation};
