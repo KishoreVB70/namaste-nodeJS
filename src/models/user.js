b3382f5ca4a5cf3d1de322c5e34f4971aa16968a
Kishore <kishore07kzr@gmail.com>
2025-01-05 09:19:39 +0530

    feat: update user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
 
 const userData = {
-    name: String,
-    email: String,
-    password: String,
-    image: String,
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
 };
 const userSchema = new mongoose.Schema(userData);
 const User = new mongoose.model("User", userSchema);
