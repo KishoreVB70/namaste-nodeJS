146f9557516174620d7a1455f0896e27a95e3816
Kishore <kishore07kzr@gmail.com>
2025-01-13 16:11:52 +0530

    feat: move response body extracter into utils

--- cookie/app/api/orders/[orderID]/capture/route.ts
+++ cookie/app/api/orders/[orderID]/capture/route.ts
@@ -1,32 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 import client from '@/lib/paypal';
-import { OrdersController, ApiError, ApiResponse, Order } from '@paypal/paypal-server-sdk';
+import { OrdersController, ApiError} from '@paypal/paypal-server-sdk';
+import { getResponseBody } from '@/lib/utils';
 
 const ordersController = new OrdersController(client);
-
-async function getResponseBody(response: ApiResponse<Order>): Promise<string> {
-  if (typeof response.body === 'string') {
-    return response.body;
-  } else if (response.body instanceof Blob) {
-    return await response.body.text();
-  } else if (response.body instanceof ReadableStream) {
-    const reader = response.body.getReader();
-    const decoder = new TextDecoder();
-    let result = '';
-    let done = false;
-
-    while (!done) {
-      const { value, done: doneReading } = await reader.read();
-      done = doneReading;
-      if (value) {
-        result += decoder.decode(value, { stream: !done });
-      }
-    }
-    return result;
-  } else {
-    throw new Error('Unsupported response body type');
-  }
-}
 
 export async function POST(request: NextRequest, { params }: { params: { orderID: string } }) {
   const { orderID } = params;
--- cookie/app/api/orders/route.ts
+++ cookie/app/api/orders/route.ts
@@ -1,35 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 import client from '@/lib/paypal';
-import { OrdersController, ApiError, CheckoutPaymentIntent, ApiResponse, Order } from '@paypal/paypal-server-sdk';
-
+import { OrdersController, ApiError, CheckoutPaymentIntent } from '@paypal/paypal-server-sdk';
+import { getResponseBody } from '@/lib/utils';
 const ordersController = new OrdersController(client);
 
 type Cart = {
   quantity: number
-}
-
-async function getResponseBody(response: ApiResponse<Order>): Promise<string> {
-  if (typeof response.body === 'string') {
-    return response.body;
-  } else if (response.body instanceof Blob) {
-    return await response.body.text();
-  } else if (response.body instanceof ReadableStream) {
-    const reader = response.body.getReader();
-    const decoder = new TextDecoder();
-    let result = '';
-    let done = false;
-
-    while (!done) {
-      const { value, done: doneReading } = await reader.read();
-      done = doneReading;
-      if (value) {
-        result += decoder.decode(value, { stream: !done });
-      }
-    }
-    return result;
-  } else {
-    throw new Error('Unsupported response body type');
-  }
 }
 
 export async function POST(request: NextRequest) {
