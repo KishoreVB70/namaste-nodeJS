5a8d93f298fed5e2d67083d44d5ed98788aa99d3
Kishore <kishore07kzr@gmail.com>
2025-01-23 16:03:25 +0530

    feat: utilize invalidate query

--- paypal/components/AI/GenerateAISound.tsx
+++ paypal/components/AI/GenerateAISound.tsx
@@ -2,12 +2,14 @@
 import React from 'react'
 import { fal } from "@fal-ai/client";
 import Funds from '@/components/dashboard/Funds';
+import { useQueryClient } from '@tanstack/react-query';
 
 fal.config({
   proxyUrl: "/api/fal",
 });
 
 function GenerateAISound() {
+  const queryClient = useQueryClient();
   async function generateSound() {
     const { data, requestId } = await fal.subscribe("fal-ai/stable-audio", {
       input: {
@@ -15,17 +17,16 @@ function GenerateAISound() {
         ...(
           {
             seconds_start: 0,
-            seconds_total: 15,
-            steps: 120
+            seconds_total: 2,
+            steps: 100
           }
         )
       },
       logs: true,
       pollInterval:3000,
     });
-
-    console.log(data);
-    console.log(requestId);
+    console.log(data, requestId);
+    queryClient.invalidateQueries({ queryKey: ['transactions'] });
   }
 
   return (
