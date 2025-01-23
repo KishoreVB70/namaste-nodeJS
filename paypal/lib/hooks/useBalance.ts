924aead8ce45457fd9f99769c5aefe0a6e7376f8
Kishore <kishore07kzr@gmail.com>
2025-01-23 11:45:33 +0530

    feat: add tanstack query

--- paypal/lib/hooks/useBalance.ts
+++ paypal/lib/hooks/useBalance.ts
@@ -1,12 +1,17 @@
 "use client"
 import { useEffect } from "react";
 import balanceStore from "@/lib/state/balanceStore";
-import { getBalance } from "@/lib/apiUtils";
+// import { getBalance } from "@/lib/apiUtils";
+import axios from "axios";
 
 function useBalance() {
   const { setBalance } = balanceStore();
   
   useEffect(() => {
+    async function getBalance() {
+      const response = await axios.get("/api/balance");
+      return response.data.balance;
+    }
     async function balance() {
       const balance = await getBalance();
       setBalance(balance);
